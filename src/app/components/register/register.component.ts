ed8c8f6884d544f7d69ff329d87686a4c6061615
Azeem Ansari <safdaransari37@gmail.com>
2020-11-22 15:41:27 +0530

    Registration form added

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  public registerForm: FormGroup;
+  public visibility: boolean = false;
+  public type = "password";
+  public Placeholder = "john.doe@gmail.com";
+
+  constructor(public router: Router, public fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.registerForm = this.fb.group({
+      displayName: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  toggle() {
+    this.visibility = !this.visibility;
+    this.type = this.visibility ? "text" : "password";
+  }
+
+  Register({value, valid}) {
+    console.log(value, valid);
   }
 
 }
